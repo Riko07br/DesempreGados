c259359a2f1406cc4cc274ea41184688893721ee
Natanael Felix <f.natanael.felix.b@gmail.com>
2023-06-21 16:49:20 -0300

    ops: move functionality from view to controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
 
@@ -15,29 +16,7 @@ use App\Models\Listing;
 */
 
 //Get all unempolyment listings
-Route::get('/', function () {
-    return view('listings', [
-        'heading' => 'Vagas de Desemprego',
-        'listings' => Listing::all()
-    ]);
-});
+Route::get('/', [ListingController::class, 'index']);
 
 //Find specific unempolyment
-// Route::get('/listing/{id}', function($id){
-//     $listing = Listing::find($id);
-
-//     if($listing){
-//         return view('listing', [            
-//             'listing' => $listing
-//         ]);
-//     }else{
-//         abort('404');
-//     }    
-// });
-
-//Find specific unempolyment
-Route::get('/listing/{listing}', function(Listing $listing){
-    return view('listing', [            
-        'listing' => $listing
-    ]);   
-});
+Route::get('/listing/{listing}', [ListingController::class, 'show']);
