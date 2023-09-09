4b271d3da7dee74ad450fb2ebb932cd2899cd0ba
Natanael Felix <f.natanael.felix.b@gmail.com>
2023-09-09 10:18:12 -0300

    style: install tailwind localy

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,24 +10,15 @@
         integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="//unpkg.com/alpinejs" defer></script>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script>
-        tailwind.config = {
-            theme: {
-                extend: {
-                    colors: {
-                        laravel: "#213363",
-                    },
-                },
-            },
-        };
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
     </script>
     <title>DesempreGados | Deixe seu gado desocupado hoje!</title>
 </head>
 
 <body class="mb-48">
-    <nav class="flex justify-between items-center mb-4">
-        <a href="/"><img class="w-24" src="{{ asset('images/logo.png') }}" alt="" class="logo" /></a>
+    <nav class="flex justify-between items-center mb-2">
+        <a href="/"><img class="w-12" src="{{ asset('images/logo.png') }}" alt="" class="logo" /></a>
         <ul class="flex space-x-6 mr-6 text-lg">
             @auth
                 <li>
@@ -57,11 +48,11 @@
             @endauth
         </ul>
     </nav>
-    <main>
+    <section>
         {{ $slot }}
-    </main>
+    </section>
     <footer
-        class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center">
+        class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-16 mt-24 opacity-90 md:justify-center">
         <p class="ml-2">Copyright &copy; 2023, All Rights reserved</p>
 
         <a href="/listing/create" class="absolute top-1/3 right-10 bg-black text-white py-2 px-5">Crie um desemprego</a>
